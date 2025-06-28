ec8dce104118dd408345d53ec2408881e104a96c
SanyBlagorodny <aseliverstov0056@gmail.com>
2025-06-28 13:26:22 +0300

    Refactoring

--- MyWeb/src/App.tsx
+++ MyWeb/src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
-import Hero from './components/Hero';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
+import Hero from './sections/Hero';
+import About from './sections/About';
+import Projects from './sections/Projects';
+import Contact from './sections/Contact';
 
 const theme = createTheme({
   palette: {
